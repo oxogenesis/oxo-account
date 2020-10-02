2f4a51f973e459ea519af0d1f0e28631babafd74
obeTvR9XDbUwquA6JPQhmbgaCCaiFa2rvf <namespacegenesis@gmail.com>
2020-10-02 17:20:32 +0800

    regx

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const oxoKeyPairs = require("oxo-keypairs")
-const Seed = oxoKeyPairs.generateSeed("any string you choose", 'secp256k1')
-const keypair = oxoKeyPairs.deriveKeypair(Seed)
-const Address = oxoKeyPairs.deriveAddress(keypair.publicKey)
-const PublicKey = keypair.publicKey
-const PrivateKey = keypair.privateKey
 
-console.log(`Seed      :${Seed}`)
-console.log(`Address   :${Address}`)
-console.log(`PublicKey :${PublicKey}`)
-console.log(`PrivateKey:${PrivateKey}`)
+while (true) {
+    let Seed = oxoKeyPairs.generateSeed("any string you choose", 'secp256k1')
+    let keypair = oxoKeyPairs.deriveKeypair(Seed)
+    let Address = oxoKeyPairs.deriveAddress(keypair.publicKey)
+    let PublicKey = keypair.publicKey
+    let PrivateKey = keypair.privateKey
+
+    //gen account like ohh...
+    //let regx = /^ohh.+$/
+    
+    //gen account like o...hh
+    //let regx = /^o.+hh$/
+    
+    //gen account like oh...h
+    //let regx = /^oh.+h$/
+    
+    let regx = /^o.+$/
+    let rs = regx.exec(Address)
+    if (rs != null) {
+        console.log(`Seed      :${Seed}`)
+        console.log(`Address   :${Address}`)
+        console.log(`PublicKey :${PublicKey}`)
+        console.log(`PrivateKey:${PrivateKey}`)
+        return
+    }
+}
